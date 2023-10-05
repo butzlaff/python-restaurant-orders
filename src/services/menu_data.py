3061039e5e0634e4067254c0c26382584eaa8bb2
butzlaff <emiliobutz@gmail.com>
2023-10-05 17:46:05 -0300

    feat: refactor requeriment 03

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -8,18 +8,16 @@ class MenuData:
         self.source_path = source_path
         self.dishes = set()
         self.load_menu_data()
+        self.dish_dict = {}
 
-    def load_menu_data(self):
+    def load_menu_data(self, source_path: str = None) -> None:
         try:
             with open(self.source_path, 'r', encoding='utf-8') as csv_file:
-                csv_reader = csv.DictReader(csv_file)
+                csv_reader = csv.reader(csv_file)
                 # dish_dict = {}
-                for row in csv_reader:
-                    dish_name = row['dish']
-                    ingredient_name = row['ingredient']
-                    recipe_amount = int(row['recipe_amount'])
-                    price = float(row['price'])
-                    print(dish_name, ingredient_name, recipe_amount, price)
+                csv_reader, *dish_dict = csv_reader
+            self.dish_dict = dish_dict
+            print(dish_dict)
         except FileNotFoundError:
             print(f"File '{self.source_path}' not found.")
 
